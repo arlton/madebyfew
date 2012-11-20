b3e5ebdb8d2fd213ad69003df55783be66c8fe25
David Hudson <im@davidhudson.me>
2012-11-19 18:00:04 -0600

    Adding combined scripts reference. This is overwritten during build process.

---
+++ js/combined-scripts.js
@@ -0,0 +1,21 @@
+function loadScript(url)
+{
+    // adding the script tag to the head as suggested before
+   var head = document.getElementsByTagName('head')[0];
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.src = url;
+
+   // fire the loading
+   head.appendChild(script);
+}
+
+loadScript('js/bootstrap-carousel.js');
+loadScript('js/bootstrap-modal.js');
+loadScript('js/bootstrap-tooltip.js');
+loadScript('js/jquery.scrollTo.min.js');
+loadScript('js/waypoints.min.js');
+loadScript('js/jquery.fitvids.js');
+$(document).ready(function() {
+	loadScript('js/general.js');
+});
