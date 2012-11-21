b117b9c671bbeb3bf0375ab7e7a59bef7d84c5bc
David Hudson <im@davidhudson.me>
2012-11-21 14:17:31 -0600

    Updated build.sh to minify jQuery. Could be a lot more efficient but will do for now. Updated distribution folder.

--- utils/build.sh
+++ utils/build.sh
@@ -29,5 +29,6 @@ echo "Minifying CSS"
 java -jar yuicompressor.jar --type css --charset utf-8 -o ../dist/css/style.css ../css/style.css
 
 echo "Minifying JS"
+java -jar compiler.jar --js ../js/jquery.js --js_output_file ../dist/js/jquery.js
 java -jar compiler.jar --js ../js/bootstrap-carousel.js ../js/bootstrap-modal.js ../js/bootstrap-tooltip.js ../js/jquery.scrollTo.min.js ../js/waypoints.min.js ../js/jquery.fitvids.js ../js/general.js --js_output_file ../dist/js/combined-scripts.js
 java -jar compiler.jar --js ../js/css3-mediaqueries.js --js_output_file ../dist/js/css3-mediaqueries.js
