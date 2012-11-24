b01d4ea6b58c8fd61840d92e35eea29abad6dd38
David Hudson <im@davidhudson.me>
2012-11-24 16:23:52 -0600

    Added easing and stellar scripts so that the working directory matches dist

--- js/combined-scripts.js
+++ js/combined-scripts.js
@@ -14,7 +14,9 @@ loadScript('js/bootstrap-carousel.js');
 loadScript('js/bootstrap-modal.js');
 loadScript('js/bootstrap-tooltip.js');
 loadScript('js/jquery.scrollTo.min.js');
+loadScript('js/jquery.stellar.js');
 loadScript('js/waypoints.min.js');
+loadScript('js/jquery.easing.1.3.js');
 loadScript('js/jquery.fitvids.js');
 $(document).ready(function() {
 	loadScript('js/general.js');
